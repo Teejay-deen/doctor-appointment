723571531ba8ef33693866367137305fcb40d944
Jimoh Tajudeen <jimoh.tajudeen1017@gmail.com>
2024-12-18 12:30:58 +0000

    refactor

--- components/SubmitButton.tsx
+++ components/SubmitButton.tsx
@@ -18,6 +18,7 @@ const SubmitButton = ({ isLoading, className, children }: ButtonProps) => {
       aria-label={isLoading ? "Loading..." : "Submit"}
     >
       {isLoading ? (
+        
         <div className="flex items-center gap-4">
           <Image
             src="/assets/icons/loader.svg"
