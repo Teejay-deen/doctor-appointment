c75829d66719e92b8f4da5abd729e530830b5966
Jimoh Tajudeen <jimoh.tajudeen1017@gmail.com>
2025-01-04 08:53:31 +0000

    validation error fixes

--- lib/validation.ts
+++ lib/validation.ts
@@ -3,7 +3,6 @@ import { z } from "zod";
 export const UserFormValidation = z.object({
   name: z
     .string()
-    
     .min(2, "Name must be at least 2 characters")
     .max(50, "Name must be at most 50 characters"),
 
